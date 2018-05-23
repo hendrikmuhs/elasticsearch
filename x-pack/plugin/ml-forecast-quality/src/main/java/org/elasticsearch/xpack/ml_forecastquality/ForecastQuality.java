e5b806404ef1dfb6803168c1eab927fc390859e8
Hendrik Muhs <hendrik.muhs@elastic.co>
2018-05-23 16:48:22 +0200

    first primitive version of the forecast evaluate plugin

---
+++ x-pack/plugin/ml-forecast-quality/src/main/java/org/elasticsearch/xpack/ml_forecastquality/ForecastQuality.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package org.elasticsearch.xpack.ml_forecastquality;
+
+import org.elasticsearch.action.ActionRequest;
+import org.elasticsearch.action.ActionResponse;
+import org.elasticsearch.cluster.metadata.IndexNameExpressionResolver;
+import org.elasticsearch.cluster.node.DiscoveryNodes;
+import org.elasticsearch.common.settings.ClusterSettings;
+import org.elasticsearch.common.settings.IndexScopedSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.settings.SettingsFilter;
+import org.elasticsearch.plugins.ActionPlugin;
+import org.elasticsearch.plugins.Plugin;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestHandler;
+import org.elasticsearch.xpack.ml_forecastquality.action.ForecastEvaluateAction;
+import org.elasticsearch.xpack.ml_forecastquality.action.TransportForecastEvaluateAction;
+import org.elasticsearch.xpack.ml_forecastquality.rest.action.RestForecastEvaluateAction;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import static java.util.Collections.singletonList;
+
+public class ForecastQuality extends Plugin implements ActionPlugin {
+
+    public static final String NAME = "forecast_quality";
+    protected final boolean enabled;
+
+    public ForecastQuality(Settings settings) {
+        this.enabled = true;
+    }
+
+    @Override
+    public List<RestHandler> getRestHandlers(final Settings settings, final RestController restController,
+            final ClusterSettings clusterSettings, final IndexScopedSettings indexScopedSettings, final SettingsFilter settingsFilter,
+            final IndexNameExpressionResolver indexNameExpressionResolver, final Supplier<DiscoveryNodes> nodesInCluster) {
+
+        return singletonList(new RestForecastEvaluateAction(settings, restController));
+    }
+
+    @Override
+    public List<ActionHandler<? extends ActionRequest, ? extends ActionResponse>> getActions() {
+
+        return singletonList(new ActionHandler<>(ForecastEvaluateAction.INSTANCE, TransportForecastEvaluateAction.class));
+    }
+}
---
+++ x-pack/plugin/ml-forecast-quality/src/main/java/org/elasticsearch/xpack/ml_forecastquality/calculator/AccuracyMeasure.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+package org.elasticsearch.xpack.ml_forecastquality.calculator;
+
+public class AccuracyMeasure {
+    private final int count;
+    private final long timeSpanInSeconds;
+    private final double meanAbsoluteError;
+    private final double meanAbsolutePercentageError;
+    private final double medianAbsolutePercentageError;
+    private final double symetricMeanAbsolutePercentageError;
+    private final double meanAbsoluteScaledError;
+
+    public AccuracyMeasure(int count, long timeSpanInSeconds, double meanAbsoluteError, double meanAbsolutePercentageError,
+            double medianAbsolutePercentageError, double symetricmeanAbsolutePercentageError, double meanAbsoluteScaledError) {
+        this.count = count;
+        this.timeSpanInSeconds = timeSpanInSeconds;
+        this.meanAbsoluteError = meanAbsoluteError;
+        this.meanAbsolutePercentageError = meanAbsolutePercentageError;
+        this.medianAbsolutePercentageError = medianAbsolutePercentageError;
+        this.symetricMeanAbsolutePercentageError = symetricmeanAbsolutePercentageError;
+        this.meanAbsoluteScaledError = meanAbsoluteScaledError;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public long getTimeSpanInSeconds() {
+        return timeSpanInSeconds;
+    }
+
+    public double getMeanAbsoluteError() {
+        return meanAbsoluteError;
+    }
+
+    public double getMeanAbsolutePercentageError() {
+        return meanAbsolutePercentageError;
+    }
+
+    public double getMedianAbsolutePercentageError() {
+        return medianAbsolutePercentageError;
+    }
+
+    public double getMeanAbsoluteScaledError() {
+        return meanAbsoluteScaledError;
+    }
+
+    public double getSymetricMeanAbsolutePercentageError() {
+        return symetricMeanAbsolutePercentageError;
+    }
+}
